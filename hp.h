65cf43156dcec8574a2f34e587502a0d88093c4b
Warren <Warren.Novascone@proton.me>
2023-11-11 00:48:12 +0000

    hp stuff

--- hp.h
+++ hp.h
@@ -1,13 +1,52 @@
 #pragma once
-#include <cstdint>
+#include "hptypes.h"
 
 class hp {
 
     public:
+        // returns true if set
+        bool setMaxHP(hptype new_max_hp){
+
+            if (new_max_hp < 1)
+                return false;
+
+            MaxHP = new_max_hp;
+
+            if (CurrentHP > MaxHP)
+                CurrentHP = MaxHP;
+
+            return true;
+        }
+
+        hptype getMaxHP(){
+        return MaxHP;
+        }
+
+        hptype getCurrentHP(){
+        return CurrentHP;
+        }
+
+        void takeDamage(hptype damage) {
+            if(damage > CurrentHP){
+                CurrentHP = 0;
+                return;
+            }
+            CurrentHP -= damage;
+        }
+
+    void heal(hptype amount){
+        if (amount + CurrentHP > MaxHP){
+            CurrentHP = MaxHP;
+            return;
+        }
+
+        CurrentHP += amount;
+    }
+
+    
 
     private:
-        std::uint16_t ShieldHP;
-        std::uint16_t CurrentHP;
-        std::uint16_t MaxHP;
+        hptype CurrentHP;
+        hptype MaxHP;
     
 };
